92302b90b8434569a546024c59b636e257c77659
matias erenchun <matiaserenchun@gmail.com>
2018-12-15 17:53:16 -0300

    pruebas de persistencia
    creamos clases para  probar la persistencia de estas en java

---
+++ pruebas de persistencia en java/src/Personaje.java
@@ -0,0 +1,59 @@
+public class Personaje
+{
+    private String name;
+    private String raza;
+    private String clase;
+    private int edad;
+
+    public Personaje(String name, String raza, String clase, int edad)
+    {
+        this.name = name;
+        this.raza = raza;
+        this.clase = clase;
+        this.edad = edad;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza)
+    {
+        this.raza = raza;
+    }
+
+    public String getClase()
+    {
+        return clase;
+    }
+
+    public void setClase(String clase)
+    {
+        this.clase = clase;
+    }
+
+    public int getEdad()
+    {
+        return edad;
+    }
+
+    public void setEdad(int edad)
+    {
+        this.edad = edad;
+    }
+
+    public String ToString()
+    {
+        return this.name+" es de la raza"+this.raza+" tiene una edad de  "+this.edad+" y es de la clase"+this.clase;
+    }
+}
